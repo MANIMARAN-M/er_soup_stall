db157f0ea828ac5642f016b8aec5351617042116
MANIMARAN-M <61526125+MANIMARAN-M@users.noreply.github.com>
2020-07-02 08:28:34 +0530

    90% Completed

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// new fullpage('#main-page', {
-//     autoScrolling: true
-// });
-
-
 const home_content = document.querySelector('.home-content');
 const home_image = document.querySelector('.home');
-const sales_content = document.querySelector('.sales-content');
+const image = document.querySelector('.re-img');
+const google_form = document.querySelector('.google-form');
 
 const t1 = new TimelineMax();
 
 t1.fromTo(home_content,
     1.5,
-    { opacity: 0, y: 30 },
+    { opacity: 0, y: 50 },
     { opacity: 1, y: 0 })
 t1.fromTo(home_image,
     1.5,
-    { opacity: 0, y: 30 },
+    { opacity: 0, y: 50 },
     { opacity: 1, y: 0 },
     "-=1.5")
-t1.fromTo(sales_content,
-    1.5,
-    { opacity: 0, y: 30 },
-    { opacity: 1, y: 0 },
-    "-=1.5");
+t1.fromTo(image,
+    .5,
+    { opacity: 0, x: -100 },
+    { opacity: 1, x: 0 })
+t1.fromTo(google_form,
+    .4,
+    { opacity: 0, x: -100 },
+    { opacity: 1, x: 0 },
+);
